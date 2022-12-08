e094e2baf26c955205d38ab84c6ea7e6fed91e9d
Hasun <whs1999kr@naver.com>
2022-12-08 13:43:43 +0900

    changed visibility of internal sorting function to private

--- src/quick_sort.rs
+++ src/quick_sort.rs
@@ -15,7 +15,7 @@ fn quicksort_recursive<T: Ord>(s: &mut [T], start: usize, end: usize) {
 }
 
 //this function will panic if s.len() < 1.
-pub fn partition<T: Ord>(s: &mut [T]) -> usize {
+fn partition<T: Ord>(s: &mut [T]) -> usize {
     let mut i = 0; //next place to store value smaller then pivot
     for j in 0..s.len() - 1 {
         //move value to left region if smaller then pivot
